1f67fc67536833ce41e3f6e37e38813b39719614
Matthew Theiley <matthewtheiley@gmail.com>
2024-12-04 23:20:26 +1030

    simplifying design for linked-list.cpp

--- Implementation/linked-list.cpp
+++ Implementation/linked-list.cpp
@@ -1,46 +1,30 @@
 #include <iostream>
-#include <memory>
-#include <initializer_list>
 
 using namespace std;
 
 template <class T>
-struct Node{
-    unique_ptr<Node<T>> next;
+struct Node {
+    Node<T>* next;
     T value;
 
     Node(T value) {
         this->value = value;
-        cout << "Node ID: " << (long) this << " created" << endl; 
-    }
-    ~Node() {
-        cout << "Node ID: " << (long) this << " destroyed" << endl; 
     }
 };
 
-template <class T>
-unique_ptr<Node<T>> gen_node(T value) {
-    return make_unique<Node<int>>(value);    
-}
-
-template <class T>
-unique_ptr<Node<T>> gen_list(initializer_list<T> list) {
-    if(list.size() == 0) return {};
-    auto dummy = gen_node(*(list.begin())); //Makes it easier to work with LL
-    Node<T>* head = dummy.get();
-    for(auto e : list) {
-        auto next = gen_node(e);
-        head->next = std::move(next);
-        head = head->next.get();
-    }
-    return std::move(dummy->next);
-}
-
 int main() {
-    auto n1 = gen_list({1, 2, 3, 4});
-    Node<int>* n = n1.get();
+    auto n1 = new Node(3);
+    auto n2 = new Node(5);
+    auto n3 = new Node(7);
+    auto n4 = new Node(9);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+
+    auto n = n1;
     while(n != nullptr) {
         cout << n->value << endl;
-        n = n->next.get();
+        n = n->next;
     }
 }
